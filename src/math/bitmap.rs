ccb04c3d3a5e7db1e561a11281927a9907c20b56
Yuchen Wang <wyc8094@gmail.com>
2020-09-11 22:28:06 +0800

    init_commit

---
+++ src/math/bitmap.rs
@@ -0,0 +1,8 @@
+// Copyright 2020 @TwoCookingMice
+
+use super::constants::{ Int, Float, Vector2f, Vector3f,
+                        FLOAT_MIN, FLOAT_MAX };
+
+#[derive(Debug, Copy)]
+pub struct Bitmap {
+}
