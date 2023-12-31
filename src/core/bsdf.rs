af8e869b49e89d048f4b6a5f16dc68317d74b9e9
Yu-Chen Wang <yuchew23@uci.edu>
2023-12-30 20:54:19 -0800

    init_lambertian_diffuse

--- src/core/bsdf.rs
+++ src/core/bsdf.rs
@@ -1,10 +1,11 @@
 // Copyright @yucwang 2023
 
 use crate::math::constants::{ Float, Vector2f, Vector3f };
+use crate::math::spectrum::RGBSpectrum;
 
 // Definitions of types used in BSDF sampling and eval 
 // processes
-pub type BSDFValue = Vector3f;
+pub type BSDFValue = RGBSpectrum;
 
 #[derive(Debug, PartialEq)]
 pub struct BSDFSampleRecord {
@@ -21,6 +22,6 @@ pub struct BSDFEvalResult {
 
 pub trait BSDF {
     fn eval(&self, sample_record: BSDFSampleRecord) -> BSDFEvalResult;
-    fn sample(&self, u1: Float, u2: Vector2f) -> BSDFSampleRecord;
-    fn sample_and_eval(&self) -> BSDFEvalResult;
+    fn sample(&self, u1: Vector2f, u2: Vector2f) -> BSDFSampleRecord;
+    fn sample_and_eval(&self, u1: Vector2f, u2: Vector2f) -> BSDFEvalResult;
 }
---
+++ src/materials/lambertian_diffuse.rs
@@ -0,0 +1,35 @@
+// Copyright @yucwang 2023
+
+use crate::core::bsdf::{BSDFSampleRecord, BSDFEvalResult, BSDF};
+use crate::core::computation_node::Computation_Node;
+use crate::math::constants::{ Float, Vector2f, Vector3f };
+use crate::math::spectrum::RGBSpectrum;
+
+pub struct LambertianDiffuseBSDF {
+    color: RGBSpectrum
+}
+
+impl Computation_Node for LambertianDiffuseBSDF {
+    fn to_string(&self) -> String {
+        String::from("LambertianDiffuseBSDF: {}")
+    }
+}
+
+impl BSDF for LambertianDiffuseBSDF {
+    fn eval(&self, sample_record: BSDFSampleRecord) -> BSDFEvalResult {
+    }
+
+    fn sample(&self, u1: Vector2f, u2: Vector2f) -> BSDFSampleRecord {
+    }
+
+    fn sample_and_eval(&self, u1: Vector2f, u2: Vector2f) -> BSDFEvalResult {
+    }
+}
+
+impl LambertianDiffuseBSDF {
+    pub fn new(rgb: RGBSpectrum) -> Self {
+        Self {
+            color: rgb,
+        }
+    }
+}
